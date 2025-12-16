6f47f1d41062ffd3566f79a69613ca5170590053
julirmatos <juli.rmatos@gmail.com>
2025-12-16 09:53:27 -0300

    Projeto Farmácia - config

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    // Deixa disponível em todo o projeto as variáveis de ambiente
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // Configuração do banco de dados
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3310,
+      username: 'root',
+      password: 'root',
+      database: 'db_seguro_vidas',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
